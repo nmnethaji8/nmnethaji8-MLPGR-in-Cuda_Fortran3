7347296d21181a81cc23cfae49f269853b8b9776
nmnethaji8 <nmnethaji8@gmail.com>
2022-01-03 10:30:10 +0530

    Resolved a bug
    Forgot to add P=PTMP line 235 in mlpgrMain.f95

--- test6.cu
+++ test6.cu
@@ -1,5 +1,5 @@
-#include <iostream>     // std::cout
-#include<fstream>
+#include <iostream> // std::cout
+#include <fstream>
 
 #include <cusp/csr_matrix.h>
 #include <cusp/precond/diagonal.h>
@@ -18,73 +18,74 @@ using namespace std;
 
 extern "C"
 {
-    void fortran_solve_csr_(int n,int m,int nnz,int *rowoffset,int *col,double *val,double *rhs,double *x)
-    {
-        // create an empty sparse matrix structure (CSR format)
-        cusp::csr_matrix<int, ValueType, MemorySpace> A(n,m,nnz);
+   void fortran_solve_csr_(int n, int m, int nnz, int *rowoffset, int *col, double *val, double *rhs, double *x)
+   {
+      // create an empty sparse matrix structure (CSR format)
+      cusp::csr_matrix<int, ValueType, MemorySpace> A(n, m, nnz);
 
-        //cout << "Row offset\n";
-        for(int i=0;i<=n;i++)
-        {
-            A.row_offsets[i] = rowoffset[i];
-            //cout << rowoffset[i] << " ";
-        }
+      // cout << "Row offset\n";
+      for (int i = 0; i <= n; i++)
+      {
+         A.row_offsets[i] = rowoffset[i];
+         // cout << rowoffset[i] << " ";
+      }
 
-        //cout << "cloumn and val\n";
-        for(int i=0;i<nnz;i++)
-        {
-            A.column_indices[i] = col[i]; A.values[i] = val[i];
+      // cout << "cloumn and val\n";
+      for (int i = 0; i < nnz; i++)
+      {
+         A.column_indices[i] = col[i];
+         A.values[i] = val[i];
 
-            //cout << col[i] << " " << val[i] << "\n";
-        }
-        
-        //cusp::print(A);
+         // cout << col[i] << " " << val[i] << "\n";
+      }
 
-        // allocate storage for solution (x) and right hand side (b)
-        cusp::array1d<ValueType, MemorySpace> X(A.num_rows, 0);
-        cusp::array1d<ValueType, MemorySpace> B(A.num_rows, 0);
+      // cusp::print(A);
 
-        for(int i=0;i<n;i++)
-        {
-            B[i] = rhs[i];
-        }
-        //cusp::print(B);
+      // allocate storage for solution (x) and right hand side (b)
+      cusp::array1d<ValueType, MemorySpace> X(A.num_rows, 0);
+      cusp::array1d<ValueType, MemorySpace> B(A.num_rows, 0);
 
-         //std::cout << typeid(A.row_offsets).name() << '\n';
-        // set stopping criteria:
-        //  iteration_limit    = 20000
-        //  relative_tolerance = 1e-15
-        //  absolute_tolerance = 1e-10
-        cusp::monitor<ValueType> monitor(B, 20000, 1e-15, 1e-10, false);
+      for (int i = 0; i < n; i++)
+      {
+         B[i] = rhs[i];
+      }
+      // cusp::print(B);
 
-        // setup preconditioner
-        /*cusp::precond::diagonal<ValueType, MemorySpace> M(A);
-        //cusp::identity_operator<ValueType, MemorySpace> M(A.num_rows, A.num_rows);
+      // std::cout << typeid(A.row_offsets).name() << '\n';
+      // set stopping criteria:
+      //  iteration_limit    = 20000
+      //  relative_tolerance = 1e-15
+      //  absolute_tolerance = 1e-10
+      cusp::monitor<ValueType> monitor(B, 20000, 1e-15, 1e-10, false);
 
-        // solve the linear system A * x = b with the BiConjugate Gradient Stabilized method
-        cusp::krylov::bicgstab(A, X, B, monitor, M);*/
+      // setup preconditioner
+      /*cusp::precond::diagonal<ValueType, MemorySpace> M(A);
+      //cusp::identity_operator<ValueType, MemorySpace> M(A.num_rows, A.num_rows);
 
-        cusp::identity_operator<ValueType, MemorySpace> M(A.num_rows, A.num_rows);
+      // solve the linear system A * x = b with the BiConjugate Gradient Stabilized method
+      cusp::krylov::bicgstab(A, X, B, monitor, M);*/
 
-        cusp::krylov::gmres(A, X, B, 50, monitor, M);
+      cusp::identity_operator<ValueType, MemorySpace> M(A.num_rows, A.num_rows);
 
-        // report solver results
-        ofstream mlpgTerOut;
-        mlpgTerOut.open("mlpgTerOut.txt",ofstream::app);
-        if (monitor.converged())
-        {
-            mlpgTerOut << " [PARACSR]\t1\t" << monitor.iteration_count() << "\t" << endl;
-         }
-         else
-         {
-            mlpgTerOut << "Solver reached iteration limit " << monitor.iteration_limit() << " before converging";
-            mlpgTerOut << " to " << monitor.relative_tolerance() << " relative tolerance " << endl;
-         }
+      cusp::krylov::gmres(A, X, B, 50, monitor, M);
 
-         mlpgTerOut.close();
-        for(int i=0;i<n;i++)
-        {
-            x[i] = X[i];
-        }
-    }
+      // report solver results
+      ofstream mlpgTerOut;
+      mlpgTerOut.open("mlpgTerOut.txt", ofstream::app);
+      if (monitor.converged())
+      {
+         mlpgTerOut << " [PARACSR]\t1\t" << monitor.iteration_count() << "\t" << endl;
+      }
+      else
+      {
+         mlpgTerOut << "Solver reached iteration limit " << monitor.iteration_limit() << " before converging";
+         mlpgTerOut << " to " << monitor.relative_tolerance() << " relative tolerance " << endl;
+      }
+
+      mlpgTerOut.close();
+      for (int i = 0; i < n; i++)
+      {
+         x[i] = X[i];
+      }
+   }
 }
