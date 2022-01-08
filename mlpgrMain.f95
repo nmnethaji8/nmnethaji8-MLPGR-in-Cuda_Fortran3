1304042dce48430c5956d5b4e22109bdefe03875
Nethaji Mandala;oe <nmnethaji8@gmail.com>
2022-01-08 17:31:50 +0530

    Exporting 8 cpu threads and considering 1 timestep

--- mlpgrMain.f95
+++ mlpgrMain.f95
@@ -68,7 +68,7 @@ PROGRAM THREED_BREAKINGWAVE
 
    I=cudaDeviceGetLimit( val, cudaLimitMallocHeapSize )
    PRINT*,"cudaLimitMallocHeapSize",val
-   val=val*(2**13)
+   val=val*(2**11)
    I = cudaDeviceSetLimit(cudaLimitMallocHeapSize,val)
    PRINT*,"cudaLimitMallocHeapSize",val
 
